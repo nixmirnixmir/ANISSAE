d3a355743166ba75a0abba4862e9036ed6c5394e
Ksenia <yellow2arachnid@gmail.com>
2026-08-27 12:50:53 +0700

    Complete project commit@

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.Title title7 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Title title8 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.accuracyChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.initialApproximationChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.yakobiLabel = new System.Windows.Forms.Label();
+            this.zeidelLabel = new System.Windows.Forms.Label();
+            this.readFileButton = new System.Windows.Forms.Button();
+            this.generateMatrixButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkingLabel = new System.Windows.Forms.Label();
+            this.ResidialNormLabel = new System.Windows.Forms.Label();
+            this.middleLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.accuracyChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.initialApproximationChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // accuracyChart
+            // 
+            this.accuracyChart.Location = new System.Drawing.Point(12, 95);
+            this.accuracyChart.Name = "accuracyChart";
+            this.accuracyChart.Size = new System.Drawing.Size(451, 372);
+            this.accuracyChart.TabIndex = 0;
+            this.accuracyChart.Text = "chart1";
+            title7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title7.Name = "Title1";
+            title7.Text = "Зависимость числа итераций от точности";
+            this.accuracyChart.Titles.Add(title7);
+            this.accuracyChart.Visible = false;
+            // 
+            // initialApproximationChart
+            // 
+            this.initialApproximationChart.Location = new System.Drawing.Point(481, 95);
+            this.initialApproximationChart.Name = "initialApproximationChart";
+            this.initialApproximationChart.Size = new System.Drawing.Size(451, 372);
+            this.initialApproximationChart.TabIndex = 1;
+            this.initialApproximationChart.Text = "chart1";
+            title8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title8.Name = "Title1";
+            title8.Text = "Зависимость числа итераций от начального приближения";
+            this.initialApproximationChart.Titles.Add(title8);
+            this.initialApproximationChart.Visible = false;
+            // 
+            // yakobiLabel
+            // 
+            this.yakobiLabel.AutoSize = true;
+            this.yakobiLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.yakobiLabel.Location = new System.Drawing.Point(12, 470);
+            this.yakobiLabel.Name = "yakobiLabel";
+            this.yakobiLabel.Size = new System.Drawing.Size(51, 20);
+            this.yakobiLabel.TabIndex = 2;
+            this.yakobiLabel.Text = "label1";
+            this.yakobiLabel.Visible = false;
+            // 
+            // zeidelLabel
+            // 
+            this.zeidelLabel.AutoSize = true;
+            this.zeidelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.zeidelLabel.Location = new System.Drawing.Point(12, 499);
+            this.zeidelLabel.Name = "zeidelLabel";
+            this.zeidelLabel.Size = new System.Drawing.Size(51, 20);
+            this.zeidelLabel.TabIndex = 3;
+            this.zeidelLabel.Text = "label1";
+            this.zeidelLabel.Visible = false;
+            // 
+            // readFileButton
+            // 
+            this.readFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.readFileButton.Location = new System.Drawing.Point(12, 5);
+            this.readFileButton.Name = "readFileButton";
+            this.readFileButton.Size = new System.Drawing.Size(451, 34);
+            this.readFileButton.TabIndex = 4;
+            this.readFileButton.Text = "Прочитать данные из файла";
+            this.readFileButton.UseVisualStyleBackColor = true;
+            this.readFileButton.Click += new System.EventHandler(this.readFileButton_Click);
+            // 
+            // generateMatrixButton
+            // 
+            this.generateMatrixButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.generateMatrixButton.Location = new System.Drawing.Point(481, 5);
+            this.generateMatrixButton.Name = "generateMatrixButton";
+            this.generateMatrixButton.Size = new System.Drawing.Size(451, 34);
+            this.generateMatrixButton.TabIndex = 5;
+            this.generateMatrixButton.Text = "Сгенерировать матрицу случайным образом";
+            this.generateMatrixButton.UseVisualStyleBackColor = true;
+            this.generateMatrixButton.Click += new System.EventHandler(this.generateMatrixButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(477, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Количество переменных:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(678, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "1000";
+            // 
+            // checkingLabel
+            // 
+            this.checkingLabel.AutoSize = true;
+            this.checkingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkingLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.checkingLabel.Location = new System.Drawing.Point(216, 71);
+            this.checkingLabel.Name = "checkingLabel";
+            this.checkingLabel.Size = new System.Drawing.Size(491, 20);
+            this.checkingLabel.TabIndex = 8;
+            this.checkingLabel.Text = "Достаточное условие сходимости метода Якоби выполняется.";
+            this.checkingLabel.Visible = false;
+            // 
+            // ResidialNormLabel
+            // 
+            this.ResidialNormLabel.AutoSize = true;
+            this.ResidialNormLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ResidialNormLabel.Location = new System.Drawing.Point(12, 525);
+            this.ResidialNormLabel.Name = "ResidialNormLabel";
+            this.ResidialNormLabel.Size = new System.Drawing.Size(38, 20);
+            this.ResidialNormLabel.TabIndex = 9;
+            this.ResidialNormLabel.Text = "Res";
+            this.ResidialNormLabel.Visible = false;
+            // 
+            // middleLabel
+            // 
+            this.middleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.middleLabel.Location = new System.Drawing.Point(477, 525);
+            this.middleLabel.Name = "middleLabel";
+            this.middleLabel.Size = new System.Drawing.Size(455, 61);
+            this.middleLabel.TabIndex = 10;
+            this.middleLabel.Text = "Среднее отношения суммы модулей недиагональных \r\nэлементов строки к модулю диагон" +
+    "ального\r\nэлемента —";
+            this.middleLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 595);
+            this.Controls.Add(this.middleLabel);
+            this.Controls.Add(this.ResidialNormLabel);
+            this.Controls.Add(this.checkingLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.generateMatrixButton);
+            this.Controls.Add(this.readFileButton);
+            this.Controls.Add(this.zeidelLabel);
+            this.Controls.Add(this.yakobiLabel);
+            this.Controls.Add(this.initialApproximationChart);
+            this.Controls.Add(this.accuracyChart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.accuracyChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.initialApproximationChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart accuracyChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart initialApproximationChart;
+        private System.Windows.Forms.Label yakobiLabel;
+        private System.Windows.Forms.Label zeidelLabel;
+        private System.Windows.Forms.Button readFileButton;
+        private System.Windows.Forms.Button generateMatrixButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label checkingLabel;
+        private System.Windows.Forms.Label ResidialNormLabel;
+        private System.Windows.Forms.Label middleLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        const string inputFilePath = @".\input.txt";
+        const string outputFilePath = @".\output.txt";
+        StreamReader reader = new StreamReader(inputFilePath);
+        double accuracy = 0;
+        int matrixSize = 0;
+        double[,] elements;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            accuracy = double.Parse(reader.ReadLine()); //чтение точности из файла
+
+            //создание области рисования графиков
+            accuracyChart.ChartAreas.Add(new ChartArea("Math functions"));
+            initialApproximationChart.ChartAreas.Add(new ChartArea("Math functions"));
+        }
+        /// <summary>
+        /// Метод GetYakobiIterations(int matrixSize, double[,] elements, double accuracy, double initialApproximation) производит поиск переменных СЛАУ методом Якоби и возвращает количество проведённых итераций.
+        /// </summary>
+        /// <param name="matrixSize">Количество уравнений системы.</param>
+        /// <param name="elements">Двумерная матрица из коэффициентов перед переменными и свободных членов.</param>
+        /// <param name="accuracy">Точность.</param>
+        /// <param name="initialApproximation">Начальное приближение.</param>
+        /// <returns>Количество итераций метода. </returns> 
+        private int GetYakobiIterations(int matrixSize, double[,] elements, double accuracy, double initialApproximation)
+        {
+            //Задаю первый массив X
+            double[] x = new double[matrixSize];
+            for (int i = 0; i < matrixSize; i++)
+                x[i] = initialApproximation;
+
+            //Вычисление новых x i-ых и проверка точности
+            double[] x_k = new double[matrixSize];
+            double sum, sumOfSqueresOfDeffernces, sumOfx_k;
+            int iterations = 0;
+            sum = 0;
+            do
+            {
+                sumOfx_k = 0;
+                iterations++;
+
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    for (int j = 0; j < matrixSize; j++)
+                        if (i != j) sum += elements[i, j] / elements[i, i] * x[j];
+                    x_k[i] = elements[i, matrixSize] / elements[i, i] - sum;
+                    sum = 0;
+                }
+
+                //вычисление наибольшей разницы между старым и новых x для проверки точности
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    sumOfSqueresOfDeffernces = Math.Abs(x_k[i] - x[i]);
+                    if (sumOfSqueresOfDeffernces > sumOfx_k) sumOfx_k = sumOfSqueresOfDeffernces;
+                }
+
+                for (int i = 0; i < matrixSize; i++) x[i] = x_k[i];
+            } while (sumOfx_k >= accuracy && iterations < 100);
+
+            return iterations;
+        }
+        /// <summary>
+        /// Метод GetYakobiSolution(int matrixSize, double[,] elements, double accuracy, double initialApproximation) производит поиск переменных СЛАУ методом Якоби и возвращает массив с решениями.
+        /// </summary>
+        /// <param name="matrixSize">Количество уравнений системы.</param>
+        /// <param name="elements">Двумерная матрица из коэффициентов перед переменными и свободных членов.</param>
+        /// <param name="accuracy">Точность.</param>
+        /// <param name="initialApproximation">Начальное приближение.</param>
+        /// <returns>Массив решений СЛАУ.</returns>          
+        private double[] GetYakobiSolution(int matrixSize, double[,] elements, double accuracy, double initialApproximation)
+        {
+            // Задаю первый массив X
+            double[] x = new double[matrixSize];
+            for (int i = 0; i < matrixSize; i++)
+                x[i] = initialApproximation;
+
+            //Вычисление новых x i-ых и проверка точности
+            double[] x_k = new double[matrixSize];
+            double sum, sumOfSqueresOfDeffernces, sumOfx_k;
+            int iterations = 0;
+            sum = 0;
+            do
+            {
+                sumOfx_k = 0;
+                iterations++;
+
+                //вычисление новых х
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    for (int j = 0; j < matrixSize; j++)
+                        if (i != j) sum += elements[i, j] / elements[i, i] * x[j];
+                    x_k[i] = elements[i, matrixSize] / elements[i, i] - sum;
+                    sum = 0;
+                }
+
+                //вычисление максимальной разницы между старым и новым х
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    sumOfSqueresOfDeffernces = Math.Abs(x_k[i] - x[i]);
+                    if (sumOfSqueresOfDeffernces > sumOfx_k) sumOfx_k = sumOfSqueresOfDeffernces;
+                }
+
+                for (int i = 0; i < matrixSize; i++) x[i] = x_k[i];
+
+            } while (sumOfx_k >= accuracy && iterations < 100);
+
+            return x_k;
+        }
+        /// <summary>
+        /// Метод GetZeidelIterations(int matrixSize, double[,] elements) производит поиск переменных СЛАУ методом Зейделя и возвращает количество итераций.
+        /// </summary>
+        /// <param name="matrixSize">Количество уравнений системы.</param>
+        /// <param name="elements">Двумерная матрица из коэффициентов перед переменными и свободных членов.</param>
+        /// <param name="accuracy">Точность.</param>
+        /// <param name="initialApproximation">Начальное приближение.</param>
+        /// <returns>Массив решений СЛАУ.</returns>        
+        private int GetZeidelIterations(int matrixSize, double[,] elements)
+        {
+            // Задаю первый массив X
+            double[] x = new double[matrixSize];
+            for (int i = 0; i < matrixSize; i++)
+                x[i] = 1;
+
+
+            double[] x_k = new double[matrixSize];
+            x.CopyTo(x_k, 0);
+
+            double sum, sumOfSqueresOfDeffernces, sumOfx_k;
+            int iterations = 0;
+            sum = 0;
+            do
+            {
+                sumOfx_k = 0;
+                iterations++;
+
+                //вычисление новых х
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    for (int j = 0; j < matrixSize; j++)
+                    {
+                        if (j < i) sum += elements[i, j] / elements[i, i] * x_k[j];
+                        else if (i != j) sum += elements[i, j] / elements[i, i] * x[j];
+                    }
+                    x_k[i] = elements[i, matrixSize] / elements[i, i] - sum;
+                    sum = 0;
+                }
+
+                //вычисление максимальной разницы между старым и новым х
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    sumOfSqueresOfDeffernces = Math.Abs(x_k[i] - x[i]);
+                    if (sumOfSqueresOfDeffernces > sumOfx_k) sumOfx_k = sumOfSqueresOfDeffernces;
+                }
+
+                for (int i = 0; i < matrixSize; i++) x[i] = x_k[i];
+
+            } while (sumOfx_k >= 0.0001 && iterations < 100);
+
+            return iterations;
+        }
+        /// <summary>
+        /// Метод readFileButton_Click(object sender, EventArgs e) обрабатывает нажатие кнопки чтения количества переменных, коэффициентов перед переменными и свободных членов из файла и выполняет вычисления.
+        /// </summary>
+        private void readFileButton_Click(object sender, EventArgs e)
+        {
+            readFileButton.Enabled = false;
+
+            matrixSize = int.Parse(reader.ReadLine()); //чтение количества переменных
+            elements = new double[matrixSize, matrixSize + 1];
+
+            List<double> buffer = new List<double>();
+            for (int i = 0; i < matrixSize; i++)
+                foreach (var number in reader.ReadLine().Split(' '))
+                    buffer.Add(double.Parse(number));
+
+            elements = new double[matrixSize, matrixSize + 1];
+            for (int i = 0; i < matrixSize; i++)
+                for (int j = 0; j < matrixSize + 1; j++)
+                    elements[i, j] = buffer[i * (matrixSize + 1) + j];
+
+            doMagic();
+        }
+        /// <summary>
+        /// Метод generateMatrixButton_Click(object sender, EventArgs e) читает из TextBox количество переменных, генерирует случайные коэффициенты перед переменными и свободные члены уравнений, после чего выполняет вычисления.
+        /// </summary>
+        private void generateMatrixButton_Click(object sender, EventArgs e)
+        {
+            matrixSize = int.Parse(textBox1.Text); //чтение количества переменных
+            elements = new double[matrixSize, matrixSize + 1];
+
+            //генерация свободных членов и коэффициентов
+            Random random = new Random();
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize + 1; j++)
+                    if (i == j) elements[i, j] = Math.Round((random.NextDouble() + 0.5) * matrixSize * 100);
+
+                for (int j = 0; j < matrixSize; j++)
+                    if (i != j) elements[i, j] = Math.Round(random.NextDouble() * 100);
+
+                elements[i, matrixSize] = Math.Round((random.NextDouble()+0.5) * matrixSize * 100);
+            }
+
+            doMagic();
+        }
+        /// <summary>
+        /// Метод doMagic() решает систему, рисует графики и вычисляет значения для исследований.
+        /// </summary>
+        private void doMagic()
+        {
+            checkingLabel.Text = "Достаточное условие сходимости метода Якоби выполняется.";
+            checkingLabel.ForeColor = Color.DarkGreen;
+
+            double sumOfStrokeElements;
+            double diagonalDominances = 0;
+            for (int i = 0; i < matrixSize; i++)
+            {
+                sumOfStrokeElements = 0;
+
+                //подсчёт суммы модулей недиагональных элементов строки
+                for (int j = 0; j < matrixSize; j++)
+                    if (i != j) sumOfStrokeElements += Math.Abs(elements[i, j]);
+
+                //сложение отношений суммы модулей недиагональных элементов строки к модулю диагонального
+                //используется для вычисления среднего отношения
+                diagonalDominances += sumOfStrokeElements / Math.Abs(elements[i, i]);
+
+                // если сумма модулей недиагональных элементов строки больше модуля диагонального, то сообщение меняется
+                if (Math.Abs(elements[i, i]) <= sumOfStrokeElements)
+                {
+                    checkingLabel.Text = "Достаточное условие сходимости метода Якоби не выполняется.";
+                    checkingLabel.ForeColor = Color.Red;
+                    break;
+                }
+            }
+
+            //Создаем и настраиваем набор точек для рисования графика
+            Series mySeriesOfPoint = new Series();
+            mySeriesOfPoint.ChartType = SeriesChartType.Line;
+            mySeriesOfPoint.ChartArea = "Math functions";
+            mySeriesOfPoint.BorderWidth = 3;
+
+            Series initialApproximationSeries = new Series();
+            initialApproximationSeries.ChartType = SeriesChartType.Line;
+            initialApproximationSeries.ChartArea = "Math functions";
+            initialApproximationSeries.BorderWidth = 3;
+
+            for (double x = 0.0001; x <= 1; x += 0.01)
+                mySeriesOfPoint.Points.AddXY(x, GetYakobiIterations(matrixSize, elements, x, 1));
+
+            for (double x = -50; x <= 100; x += 1)
+                initialApproximationSeries.Points.AddXY(x, GetYakobiIterations(matrixSize, elements, accuracy, x));
+            
+            //Добавляем созданный набор точек в Chart
+            accuracyChart.Series.Add(mySeriesOfPoint);
+            initialApproximationChart.Series.Add(initialApproximationSeries);
+
+            yakobiLabel.Text = $"Количество итераций методом Якоби при точности, равной {accuracy}, и начальному приближению, равном 1 — {GetYakobiIterations(matrixSize, elements, 0.0001, 1)}.";
+            zeidelLabel.Text = $"Количество итераций методом Зейделя при точности, равной {accuracy}, и начальному приближению, равном 1 — {GetZeidelIterations(matrixSize, elements)}.";
+
+            //вывод решений в файл
+            double[] x_k = GetYakobiSolution(matrixSize, elements, accuracy, 1);
+            StreamWriter writer = new StreamWriter(outputFilePath, false);
+            writer.Write("Решение системы линейных уравнений: ");
+            for (int i = 0; i < matrixSize; i++)
+                writer.WriteLine($"x{i + 1} = {(Math.Round(x_k[i], 4))}");
+            writer.Close();
+
+            //подсчёт нормы невязки как максимальной невязки
+            double residialNorm = 0;
+            double residialVector;
+            double sum = 0;
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize; j++)
+                    sum += x_k[j] * elements[i, j];
+                residialVector = elements[i, matrixSize] - sum;
+                if (residialVector > residialNorm) residialNorm = residialVector;
+                sum = 0;
+            }
+            ResidialNormLabel.Text = $"Норма невязки — {residialNorm}.";
+
+            middleLabel.Text = $"Среднее отношения суммы модулей недиагональных элементов строки к модулю диагонального элемента — {diagonalDominances / matrixSize}.";
+
+            accuracyChart.Visible = true;
+            initialApproximationChart.Visible = true;
+            yakobiLabel.Visible = true;
+            zeidelLabel.Visible = true;
+            checkingLabel.Visible = true;
+            ResidialNormLabel.Visible = true;
+            middleLabel.Visible = true;
+        }
+    }
+}
